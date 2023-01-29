93240d1900e20112e1347eec60e7471efd6c2d19
toward-the-test <towardthebestprogrammer@gmail.com>
2023-01-30 07:31:30 +0900

    백준 1008 번 추가

---
+++ baekjoon/1008.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm> // find_if
+
+bool isNumber(const std::string &s) {
+    // 양의 정수 음의 정수를 처리해야한다.
+    auto start = s.begin();
+    if (s.c_str()[0] == '+') {
+        start++;
+    } else if (s.c_str()[0] == '-') {
+        start++;
+    }
+
+    // find_if는 찾으려는 값에 == 연산자가 없는 경우에 사용한다.
+    return !s.empty() && std::find_if(start, s.end(), [](unsigned char c) {
+                             return !std::isdigit(c);
+                         }) == s.end();
+}
+
+int main(int argc, const char *argv[]) {
+    try
+    {
+        std::string a , b;
+        std::cin >> a >> b;
+
+        if (isNumber(a) && isNumber(b)) {
+            std::cout << std::fixed;
+            std::cout.precision(9);
+            std::cout << std::stod(a) / std::stod(b);
+        }
+        return 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return 0;
+}
